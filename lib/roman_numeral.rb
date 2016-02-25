cfd144c6af3ea61bc4ca6279f013557298a32758
Steven Laroche <stevenplaroche@gmail.com>
2016-02-25 07:06:46 -0700

    Implements basic add functionality.  Still needs work.

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,53 +1,224 @@
+
 class RomanNumeral
-  # The order reflects the order the numerals should appear from left to right
-  @@ordered_mapping = %w(M CM D CD C XC L XL X IX V IV I)
+  class Numeral
+    attr_accessor :count, :numeral, :next_numeral, :next_numeral_combine_number, :order, :number, :exploded_numeral, :number_to_explode, :combines_with, :combines_into
 
-  # The next highest numeral for when we need to combined 5 X into an L
-  @@next_highest_mapping = {
-      'D' => ['M', 2],
-      'C' => ['D', 5],
-      'L' => ['C', 2],
-      'X' => ['L', 5],
-      'V' => ['X', 2],
-      'I' => ['V', 5]
-  }
+    def initialize(numeral)
+      @count = 0
+      send("new_#{numeral.downcase}")
+    end
 
-  # Straight arabic amounts.
-  @@roman_to_arabic_mapping = {
-      'M' => 1000,
-      'CM' => 900,
-      'D' => 500,
-      'CD' => 400,
-      'C' => 100,
-      'XC' => 90,
-      'L' => 50,
-      'XL' => 50,
-      'X' => 10,
-      'IX' => 9,
-      'V' => 5,
-      'IV' => 4,
-      'I' => 1,
-  }
+    def new_m
+      @numeral = 'M'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 0
+      @number = 1000
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+    end
 
-  @@exploded_mapping = {
-      'IV' => 'IIII',
-      'IX' => 'VIIII',
-      'XL' => 'XXXX',
-      'XC' => 'LXXXX',
-      'CD' => 'CCCC',
-      'CM' => 'DCCCC',
-  }
+    def new_cm
+      @numeral = 'CM'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 1
+      @number = 900
+      @exploded_numeral = 'C'
+      @number_to_explode = 9
+    end
+
+    def new_d
+      @numeral = 'D'
+      @next_numeral = 'M'
+      @next_numeral_combine_number = 2
+      @order = 3
+      @number = 500
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+      @combines_with = 'CD'
+      @combines_into = 'CM'
+    end
+
+    def new_cd
+      @numeral = 'CD'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 4
+      @number = 400
+      @exploded_numeral = 'C'
+      @number_to_explode = 4
+      @combines_with = 'D'
+      @combines_into = 'CM'
+    end
+
+    def new_c
+      @numeral = 'C'
+      @next_numeral = 'D'
+      @next_numeral_combine_number = 5
+      @order = 5
+      @number = 100
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+    end
+
+    def new_xc
+      @numeral = 'XC'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 6
+      @number = 90
+      @exploded_numeral = 'X'
+      @number_to_explode = 9
+    end
+
+    def new_l
+      @numeral = 'L'
+      @next_numeral = 'C'
+      @next_numeral_combine_number = 2
+      @order = 7
+      @number = 50
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+      @combines_with = 'XL'
+      @combines_into = 'XC'
+    end
+
+    def new_xl
+      @numeral = 'XL'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 8
+      @number = 40
+      @exploded_numeral = 'X'
+      @number_to_explode = 4
+      @combines_with = 'L'
+      @combines_into = 'XC'
+    end
+
+    def new_x
+      @numeral = 'X'
+      @next_numeral = 'L'
+      @next_numeral_combine_number = 5
+      @order = 9
+      @number = 10
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+    end
+
+    def new_ix
+      @numeral = 'IX'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 10
+      @number = 9
+      @exploded_numeral = 'I'
+      @number_to_explode = 9
+    end
+
+    def new_v
+      @numeral = 'V'
+      @next_numeral = 'X'
+      @next_numeral_combine_number = 2
+      @order = 11
+      @number = 5
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+      @combines_with = 'IV'
+      @combines_into = 'IX'
+    end
+
+    def new_iv
+      @numeral = 'IV'
+      @next_numeral = nil
+      @next_numeral_combine_number = nil
+      @order = 12
+      @number = 4
+      @exploded_numeral = 'I'
+      @number_to_explode = 4
+      @combines_with = 'V'
+      @combines_into = 'IX'
+    end
+
+    def new_i
+      @numeral = 'I'
+      @next_numeral = 'V'
+      @next_numeral_combine_number = 5
+      @order = 13
+      @number = 1
+      @exploded_numeral = @numeral
+      @number_to_explode = 1
+      @combines_with = 'I'
+      @combines_into = 'IV'
+    end
+
+    def get_exploded
+      if @exploded_numeral.nil?
+        return nil
+      end
+      number = @number_to_explode * @count
+      exploded = Numeral.new @exploded_numeral
+      exploded.count = number
+      @count -= number
+      return exploded
+    end
+
+    def get_next
+      if @next_numeral.nil?
+        return nil
+      end
+      number = @count / @next_numeral_combine_number
+      if number >= 1
+        new_numeral = Numeral.new @next_numeral
+        new_numeral.count += number
+        @count -= number
+        return new_numeral
+      end
+    end
+
+    def get_subtractive
+      return @combines_with
+    end
+
+    def get_subtractive_combination
+      return @combines_into
+    end
+
+    def to_i
+      @count * @number
+    end
+
+    def to_s
+      @numeral * @count
+    end
+  end
+
 
 
   def initialize(roman_numeral)
+    @numerals = {
+        'M' => Numeral.new('M'),
+        'CM' => Numeral.new('CM'),
+        'D' => Numeral.new('D'),
+        'CD' => Numeral.new('CD'),
+        'C' => Numeral.new('C'),
+        'XC' => Numeral.new('XC'),
+        'L' => Numeral.new('L'),
+        'XL' => Numeral.new('XL'),
+        'X' => Numeral.new('X'),
+        'IX' => Numeral.new('IX'),
+        'V' => Numeral.new('V'),
+        'IV' => Numeral.new('IV'),
+        'I' => Numeral.new('I')
+    }
     @original_numeral = roman_numeral
-    parsed_numerals = parse_numerals(roman_numeral)
-    puts "parsed numerals #{parsed_numerals}"
-    exploded_numerals = exploded(parsed_numerals)
-    puts "exploded numerals #{exploded_numerals}"
-    replaced_numerals = replace_with_larger(exploded_numerals)
-    puts "replaced numerals #{replaced_numerals}"
-    @numerals = order_numerals(replaced_numerals)
+    parse_numerals(roman_numeral)
+    # puts "parsed numerals #{parsed_numerals}"
+    # @exploded = explode_substitives(parsed_numerals)
+    # puts "exploded numerals #{@exploded}"
+    # replaced_numerals = replace_with_larger(@exploded)
+    # puts "replaced numerals #{replaced_numerals}"
+    # @numerals = order_numerals(replaced_numerals)
 
     # puts "original: #{@original_numeral}"
     # puts "ordered: #{self.to_s}"
@@ -55,124 +226,100 @@ class RomanNumeral
 
   private def parse_numerals(numeral)
     number = 0
-    skip_numeral = false
+    skip_character = false
     unprocessed_numerals = numeral.split ''
-    numerals = []
+    processed_numerals = []
     (0..unprocessed_numerals.length - 1).each do |pos|
       next_pos = pos + 1
       numeral = unprocessed_numerals[pos]
-      if skip_numeral
-        skip_numeral = false
+      if skip_character
+        skip_character = false
         next
       end
 
       double_numeral = unprocessed_numerals.values_at(pos, next_pos).join('')
-      double_mapping = @@roman_to_arabic_mapping[double_numeral]
-      unless double_mapping.nil?
-        numerals.push(double_numeral)
-        skip_numeral = true
+      double_mapping = @numerals[double_numeral]
+      if double_mapping.nil?
+        # numerals.push(numeral)
+        @numerals[numeral].count += 1
       else
-        numerals.push(numeral)
+        @numerals[double_numeral].count += 1
+        skip_character = true
       end
     end
-    # puts "Final amount #{amount}"
-    return numerals
   end
 
-  private def order_numerals(numerals)
-    ordered_numerals = numerals.sort_by { |x| @@ordered_mapping.index(x)}
-    return ordered_numerals
-  end
-
-  private def replace_with_larger(numerals)
-    number_of_each_numeral = numerals.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
-    number_of_each_numeral.reverse_each do |k,v|
-      next_highest = @@next_highest_mapping[k]
-      if next_highest.nil?
-        next
-      end
-      next_highest_numeral = next_highest[0]
-      next_highest_multiplier = next_highest[1]
-      amount_extra = v / next_highest_multiplier
-      if amount_extra > 0
-        number_of_each_numeral[next_highest_numeral] += amount_extra
-        next_highest_numeral[k] = v - (amount_extra * next_highest_multiplier)
-      end
-    end
-    replaced_numerals = expand_numerals_by_count(number_of_each_numeral)
-    return replaced_numerals
-  end
-
-  private def expand_numerals_by_count(numerals)
-    replaced_numerals = []
-    numerals.each do |k, v|
-      # not ideal
-      numeral = (k * v).split('')
-      puts "numeral #{numeral}"
-      replaced_numerals.push(*numeral)
-    end
-    replaced_numerals
-  end
-
-  private def exploded(numerals)
-    exploded = []
-    numerals.each do |i|
-      exploded_numeral = @@exploded_mapping.fetch(i, default=i).split
-      exploded.push(*exploded_numeral)
-    end
-    return exploded
-  end
-
-  # TODO: This method cannot run before replace_with_larger or bad things might happen such as VV turning into something weird.
-  private def unexplode(numerals)
-    number_of_each_numeral = numerals.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
-    # We need to reverse the order of the numerals so that we can start at I and go from there.
-    @@ordered_mapping.reverse_each do |i|
-      amount = number_of_each_numeral[i]
-      next_highest = @@next_highest_mapping[i]
-      if amount == 0 or next_highest.nil?
-        next
-      end
-      puts "amount of #{i} is #{amount}"
-      next_highest_numeral = next_highest[0]
-      # The subtractives start at one less than the next highest numeral
-      combining_minimum_amount = next_highest[1] - 1
-      # If we have less than the combining amount, skip
-      # Since we have to run replace_with_larger first, so we shouldn't have more than this combining_minimum_amount
-      if amount < combining_minimum_amount
-        next
-      end
-
-      number_of_next_highest = number_of_each_numeral[next_highest_numeral]
-      # Remove the combining amount and one from the next highest amount
-      if number_of_next_highest > 0
-        number_of_each_numeral[next_highest_numeral] -= 1
-      end
-      number_of_each_numeral[i] -= combining_minimum_amount
-      # Get the proper map
-      exploded_numeral = next_highest_numeral + (i * amount)
-      substutive_map = @@exploded_mapping.key(exploded_numeral)
-      number_of_each_numeral[substutive_map] = 1
-    end
-    # puts "Unexploded numerals: #{number_of_each_numeral}"
-    replaced_numerals = expand_numerals_by_count(number_of_each_numeral)
-    return replaced_numerals
+  def numerals
+    Marshal.load(Marshal.dump(@numerals))
   end
 
   def +(other)
     # 1. substitute the exploded (subtractive) values
+    self_numerals = self.numerals
+    self_numerals.values.each do |x|
+      next_numeral = x.get_exploded
+      unless next_numeral.nil?
+        self_numerals[next_numeral.numeral].count += next_numeral.count
+      end
+    end
+    other_numerals = other.numerals
+    other_numerals.values.each do |x|
+      next_numeral = x.get_exploded
+      unless next_numeral.nil?
+        other_numerals[next_numeral.numeral].count += next_numeral.count
+      end
+    end
+
     # 2. catenate the values
+    catenated = self_numerals
+    other.numerals.each do |key, value|
+      catenated[key].count += value.count
+    end
+
     # 3. sort the symbols largest <= left
-    # 4. start at right end and combine any of the same symbols that can make
+    combined_numerals = catenated.sort_by { |k, v| v.order }
+    # sorted_numerals = order_numerals(catenated)
+    # 4. start at right end and combine any of the same symbols
     #    that can be combined into a larger one
+    catenated.values.sort_by { |x| x.order }.reverse_each do |v|
+      next_numeral = v.get_next
+      unless next_numeral.nil?
+        catenated[next_numeral.numeral] += count
+      end
+    end
+    # combined = replace_with_larger(sorted_numerals)
     # 5. substitute any subtractives
+    catenated.values.sort_by { |x| x.order }.reverse_each do |v|
+      # Special case I combines with itself 4 times to get IV
+      if v.numeral == 'I'
+        total_ivs = v.count / 4
+        if total_ivs >= 1
+          v.count -= total_ivs * 4
+          catenated['IV'].count += total_ivs
+        end
+      else
+        subtractive = v.get_subtractive
+        unless subtractive.nil?
+          second_subtractive = catenated[subtractive]
+          total_combination = [v.count, second_subtractive.count].min
+          if total_combination >= 1
+            catenated[v.get_subtractive_combination].count += total_combination
+            v.count -= total_combination
+            second_subtractive.count -= total_combination
+          end
+        end
+      end
+    end
+
+    # unexploded = order_numerals(unexplode(combined))
+    return RomanNumeral.new catenated.values.select { |x| x.count >= 1 }.sort_by { |x| x.order }.join
   end
 
   def to_i
-    return @numerals.inject(0) { |sum, x| sum + @@roman_to_arabic_mapping[x] }
+    @numerals.values.map(&:to_i).reduce(&:+)
   end
 
   def to_s
-    return order_numerals(unexplode(@numerals)).join
+    return @numerals.values.select { |x| x.count > 0 }.sort_by { |x| x.order }.join
   end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -61,4 +61,47 @@ describe RomanNumeral do
       end
     end
   end
+
+  describe '.+' do
+    context 'given V + I' do
+      it 'should return VI' do
+        numeral1 = RomanNumeral.new 'V'
+        numeral2 = RomanNumeral.new 'I'
+
+        added_numeral = numeral1 + numeral2
+        expect(added_numeral.to_s).to eql('VI')
+      end
+    end
+
+    context 'given XI + V' do
+      it 'should return XVI' do
+        numeral1 = RomanNumeral.new 'XI'
+        numeral2 = RomanNumeral.new 'V'
+
+        added_numeral = numeral1 + numeral2
+        expect(added_numeral.to_s).to eql('XVI')
+      end
+    end
+
+    context 'given XVI + III' do
+      it 'should return XIX' do
+        numeral1 = RomanNumeral.new 'XVI'
+        numeral2 = RomanNumeral.new 'III'
+
+        added_numeral = numeral1 + numeral2
+        expect(added_numeral.to_s).to eql('XIX')
+      end
+    end
+
+    context 'given CXLIV + DCCCLV' do
+      it 'should return CMXCIX' do
+        numeral1 = RomanNumeral.new 'CXLIV'
+        numeral2 = RomanNumeral.new 'DCCCLV'
+
+        added_numeral = numeral1 + numeral2
+        expect(added_numeral.to_s).to eql('CMXCIX')
+      end
+    end
+
+  end
 end
